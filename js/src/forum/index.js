67ff15eb919e2fce0cc88b25f6049cc56a48b0ba
William Di Cicco <nearata@protonmail.com>
2020-10-24 17:59:49 +0200

    Update to beta 14

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,12 +3,12 @@ import { extend } from 'flarum/extend';
 import DiscussionPage from 'flarum/components/DiscussionPage';
 
 app.initializers.add('nearata/flarum-ext-remove-pane', () => {
-    extend(DiscussionPage.prototype, 'init', () => app.pane.disable());
+    extend(DiscussionPage.prototype, 'oninit', () => app.pane.disable());
 
-    extend(DiscussionPage.prototype, 'view', vnode => {
-        for (const [i, v] of vnode.children.entries()) {
-            if (v.attrs && v.attrs.className === 'DiscussionPage-list') {
-                vnode.children.splice(i, 1, '');
+    extend(DiscussionPage.prototype, 'oncreate', function () {
+        for (const i of this.element.children) {
+            if (i.className === 'DiscussionPage-list') {
+                i.remove();
             }
         }
     });
